f3a7a83b829a1335385430fd4809c582d437bef9
petter.ku <petter.ku@yahoo.com>
2026-04-09 21:29:29 +0800

    add unit test for ValidationHelper

---
+++ SkyGate-ADONET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkyGate-ADONET.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkyGate-ADONET.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff71e488-3d49-486d-9f32-5e8b26bd9710")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SkyGate-ADONET.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkyGate_ADONET.Utilities; // 確保引用了你的工具類別
+
+namespace SkyGate_ADONET.Tests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        [TestMethod]
+        public void Test_Email格式正確_應回傳True()
+        {
+            bool result = ValidationHelper.ValidateEmail("service@skygate.com");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_Email格式錯誤_應回傳False()
+        {
+            bool result = ValidationHelper.ValidateEmail("bad-email-format");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_工號格式正確_應回傳True()
+        {
+            // 根據你的 Regex: ^E\d{3}$
+            bool result = ValidationHelper.ValidateEmployeeId("E123");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_工號格式錯誤_應回傳False()
+        {
+            bool result = ValidationHelper.ValidateEmployeeId("A123"); // 開頭錯
+            Assert.IsFalse(result);
+        }
+    }
+}
